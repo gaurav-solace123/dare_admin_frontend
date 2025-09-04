6bffb93980b7ad88268429870325b4f23cafddef
gauravjadhav.solace <gauravjadhav.solace@gmail.com>
2025-09-04 10:12:29 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// Deliberate syntax error to trigger CodeRabbit check failure
+const thisWillBreak = { 
+  key1: "value1",
+  key2: "value2"  // Missing comma
+  key3: "value3"
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Suspense>
     <BrowserRouter>
